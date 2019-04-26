8858ba6a1ca15ca019bedf0c4b3b9a7f7245fa3f
Nicolas Gross <nicolas@fam-gross.org>
2019-04-26 15:51:55 +0200

    Use fzf in fish

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -4,12 +4,14 @@ function fish_user_key_bindings
 	bind -M insert \cj down-or-search
 	bind -M insert \ck up-or-search
 	
-	# use CTRL-f to accept suggestion and enter
+	# use CTRL-f to accept suggestion
 	bind \cf 'commandline -f accept-autosuggestion'
 	bind -M insert \cf 'commandline -f accept-autosuggestion'
 
-	# use CTRL-g to accept suggestion and enter
+	# use CTRL-g to accept next suggested word
 	bind \cg 'commandline -f forward-word'
 	bind -M insert \cg 'commandline -f forward-word'
 end
+
+fzf_key_bindings
 
